16f778358823918b470a16a32b4c6def964fd9fd
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2025-07-24 10:58:53 +0700

    done crud

--- app/crud/user.py
+++ app/crud/user.py
@@ -11,3 +11,22 @@ def create_user(db: Session, user: UserCreate):
 
 def get_users(db: Session):
     return db.query(User).all()
+
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def update_user(db: Session, user_id: int, updated: UserCreate):
+    user = get_user_by_id(db, user_id)
+    if user:
+        user.name = updated.name
+        user.email = updated.email
+        db.commit()
+        db.refresh(user)
+    return user
+
+def delete_user(db: Session, user_id: int):
+    user = get_user_by_id(db, user_id)
+    if user:
+        db.delete(user)
+        db.commit()
+    return user
--- app/routers/user.py
+++ app/routers/user.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.schemas import user as user_schema
 from app.database import SessionLocal
 from app.crud import user as user_crud
+from fastapi import HTTPException
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -20,3 +21,24 @@ def read_users(db: Session = Depends(get_db)):
 @router.post("/", response_model=user_schema.UserResponse)
 def create_new_user(user: user_schema.UserCreate, db: Session = Depends(get_db)):
     return user_crud.create_user(db, user)
+
+@router.get("/{user_id}", response_model=user_schema.UserResponse)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    user = user_crud.get_user_by_id(db, user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@router.put("/{user_id}", response_model=user_schema.UserResponse)
+def update_user(user_id: int, updated_user: user_schema.UserCreate, db: Session = Depends(get_db)):
+    user = user_crud.update_user(db, user_id, updated_user)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@router.delete("/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    deleted = user_crud.delete_user(db, user_id)
+    if deleted is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"message": "User deleted"}
